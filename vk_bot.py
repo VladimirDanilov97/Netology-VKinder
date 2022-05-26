6435e9d66503a480ed7516e29886e8ea9803948b
VladimirDanilov97 <dan1997ford@yandex.ru>
2022-05-26 10:52:34 +0300

    Refactor vk_bot

--- vk_bot.py
+++ vk_bot.py
@@ -1,6 +1,5 @@
 import re
-import logging
-from pprint import pprint
+from random import shuffle
 from config import GROUP_TOKEN, USER_TOKEN
 from vk_api.longpoll import VkEventType
 from keyboards import bot_keyboard, search_option_keyboard
@@ -10,9 +9,11 @@ class MyBot(MyBotFunctions):
 
     def __init__(self, token, user_token) -> None:
         super().__init__(token, user_token)
-        self.offset = {}
-        self.suitable_users_gen = {}
+     
+     
+        self.lists_users_to_send = {}
         self.last_search_params = {}
+        self.user_search_index = {}
 
     def search_command_handler(self, event):
 
@@ -24,54 +25,68 @@ class MyBot(MyBotFunctions):
                 'age_from': int(user_search_params[2]),
                 'age_to': int(user_search_params[3])
             }
-            self.offset[event.user_id] = 0
-            self.suitable_users_gen[event.user_id] = self.find_suitable_users(
+            
+            for age in range(int(user_search_params[2]), int(user_search_params[3])+1):
+                if self.lists_users_to_send.get(event.user_id):
+                    self.lists_users_to_send[event.user_id].append(self.find_suitable_users(
                                                         self.last_search_params[event.user_id]['city_id'], 
                                                         self.last_search_params[event.user_id]['sex_id'],
-                                                        self.last_search_params[event.user_id]['age_from'], 
-                                                        self.last_search_params[event.user_id]['age_to'],
-                                                        self.offset[event.user_id]
+                                                        age_from = age,
+                                                        age_to = age
+                                                        ))
+                else:
+                    self.lists_users_to_send[event.user_id]= self.find_suitable_users(
+                                                        self.last_search_params[event.user_id]['city_id'], 
+                                                        self.last_search_params[event.user_id]['sex_id'],
+                                                        age_from = age,
+                                                        age_to = age
                                                         )
+                    self.user_search_index[event.user_id] = 0
+            shuffle(self.lists_users_to_send[event.user_id])
             event.text = 'следующий'
             return self.search_command_handler(event)
         
         elif event.text.lower() == 'следующий':
             try:
-                user_to_send = next(self.suitable_users_gen[event.user_id])
+                user_to_send = self.lists_users_to_send[event.user_id][self.user_search_index[event.user_id]]   
+                self.user_search_index[event.user_id] += 1
+            
             except StopIteration:
-                self.offset[event.user_id] += self._count
-                self.suitable_users_gen[event.user_id] = self.find_suitable_users(
-                                                            self.last_search_params[event.user_id]['city_id'], 
-                                                            self.last_search_params[event.user_id]['sex_id'],
-                                                            self.last_search_params[event.user_id]['age_from'], 
-                                                            self.last_search_params[event.user_id]['age_to'],
-                                                            self.offset[event.user_id]
-                                                            )
+                new_state = 'None'
+                self.db.update_user_state(event.user_id, new_state=new_state)
+                self.write_msg(event.user_id, 'Больше нет пользователей подходящих по запросу', bot_keyboard)
+                event.text = 'начать поиск'
+                return self.user_command_handler(event)
+            if not self.db.is_user_in_black_list(event.user_id, user_to_send['id']):
                 try:
-                    user_to_send = next(self.suitable_users_gen[event.user_id])
-                except StopIteration:
-                    new_state = 'None'
-                    self.db.update_user_state(event.user_id, new_state=new_state)
-                    self.write_msg(event.user_id, 'Больше нет пользователей подходящих по запросу', bot_keyboard)
-                    event.text = 'начать поиск'
-                    return self.user_command_handler(event)
-            try:
-                user_photo = self.get_top_3_photo(user_to_send['id']) 
-            except:
+                    user_photo = self.get_top_3_photo(user_to_send['id']) 
+                except:
+                    event.text = 'следующий'
+                    return self.search_command_handler(event)
+
+                if user_to_send.get('last_seen'):
+                    last_seen = date.fromtimestamp(int(user_to_send['last_seen']['time'])).strftime('%d.%m.%Y')
+                    last_seen = 'Заходил в последний раз: ' + last_seen + '\n'
+                else:
+                    last_seen = ''
+
+                message = f"{user_to_send['first_name']} {user_to_send['last_name']}\n{last_seen}\
+                            https://vk.com/id{user_to_send['id']}"
+                            
+                self.send_media(user_id=event.user_id,
+                                media_owner_id=user_to_send['id'],
+                                media_ids=user_photo,
+                                message=message,
+                                keyboard=search_option_keyboard)
+            else:
                 event.text = 'следующий'
                 return self.search_command_handler(event)
 
-            last_seen = date.fromtimestamp(int(user_to_send['last_seen']['time'])).strftime('%d.%m.%Y')
-            message = f"{user_to_send['first_name']} {user_to_send['last_name']}\n\
-                        Заходил в последний раз: {last_seen}\n\
-                        https://vk.com/id{user_to_send['id']}"
-                        
-            self.send_media(user_id=event.user_id,
-                            media_owner_id=user_to_send['id'],
-                            media_ids=user_photo,
-                            message=message,
-                            keyboard=search_option_keyboard)
-
+        elif event.text.lower() == 'не показывать больше':
+            self.db.add_to_black_list(user_id=event.user_id, blocked_user_id=self.lists_users_to_send[event.user_id][self.user_search_index[event.user_id]-1])
+            event.text = 'следующий'
+            return self.search_command_handler(event)
+        
         elif event.text.lower() == 'назад к меню':
             new_state = 'None'
             self.db.update_user_state(event.user_id, new_state)
@@ -84,7 +99,10 @@ class MyBot(MyBotFunctions):
                 self.write_msg(event.user_id, 'Вы уже зарегистрированы', bot_keyboard)
             else:
                 self.register_user(event.user_id)
-                self.write_msg(event.user_id, 'Вы зарегистрированы', bot_keyboard)
+                message = 'Вы зарегистрированы\n\
+                           Если знаете id нужного города нажмите начать поиск.\n\
+                           Если нет, нажмите "Найти id города" и отправьте название города следующим сообщением'
+                self.write_msg(event.user_id, message, bot_keyboard)
                
         elif request == 'найти id города':
             new_state = 'найти id города'
@@ -139,7 +157,6 @@ class MyBot(MyBotFunctions):
 
 if __name__ == '__main__':
     bot = MyBot(GROUP_TOKEN, USER_TOKEN)
-    try:
-        pprint(bot.start_listen())
-    except KeyboardInterrupt:
-        pprint(bot.offset)
+
+    bot.start_listen()
+
