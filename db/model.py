052bf7f5401e6afb119d2ef8be2ecf35dea0e024
VladimirDanilov97 <dan1997ford@yandex.ru>
2022-05-26 10:44:26 +0300

    add new tables for datacache

--- db/model.py
+++ db/model.py
@@ -29,5 +29,24 @@ user_state_machine = Table('user_state_machine', metadata,
     Column('state', String(80), default='None')
     )
 
+user_search_index = Table('user_search_index', metadata,
+    Column('user_id', Integer, primary_key=True),
+    Column('search_index', Integer),
+    )
+
+user_search_index = Table('last_search_params', metadata,
+    Column('user_id', Integer, primary_key=True),
+    Column('city_id', Integer),
+    Column('sex_id', Integer),
+    Column('age_from', Integer),
+    Column('age_to', Integer),
+    )
+    
+list_of_user_to_send = Table('list_of_user_to_send', metadata,
+    Column('id', Integer, primary_key=True),
+    Column('user_id', Integer),
+    Column('user_id_to_send', Integer),
+    )
+
 if __name__ == '__main__':
     metadata.create_all(engine)
